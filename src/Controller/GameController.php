94487dd5ae17ef400f0a328be83a85f0be8eb4c1
AGlanborgBTH <93671007+AGlanborgBTH@users.noreply.github.com>
2022-04-16 12:37:44 +0200

    /game & /game/doc
    Starting- and documentation-page setup

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -13,12 +13,36 @@ class GameController extends AbstractController
 {
     /**
      * @Route(
+     *      "/game",
+     *      name="game-home",
+     *      methods={"GET","HEAD"}
+     * )
+     */
+    public function game_home(): Response
+    {
+        return $this->render('game/index.html.twig');
+    }
+
+    /**
+     * @Route(
      *      "/game/card",
      *      name="game-card-home",
      *      methods={"GET","HEAD"}
      * )
      */
     public function card_home(): Response
+    {
+        return $this->render('game/card.html.twig');
+    }
+
+    /**
+     * @Route(
+     *      "/game/doc",
+     *      name="game-doc-home",
+     *      methods={"GET","HEAD"}
+     * )
+     */
+    public function doc_home(): Response
     {
         return $this->render('game/card.html.twig');
     }
