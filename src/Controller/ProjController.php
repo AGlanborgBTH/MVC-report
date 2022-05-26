757646e893ebf83cc055f16942d62a7160b87226
AGlanborgBTH <93671007+AGlanborgBTH@users.noreply.github.com>
2022-05-26 18:53:09 +0200

    db
    Database implemented into controller

--- src/Controller/ProjController.php
+++ src/Controller/ProjController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Logins;
+use App\Entity\History;
+use App\Repository\LoginsRepository;
+use App\Repository\HistoryRepository;
 
 class ProjController extends AbstractController
 {
@@ -17,9 +22,29 @@ class ProjController extends AbstractController
      *      methods={"GET","HEAD"}
      * )
      */
-    public function home(): Response
+    public function home(
+        SessionInterface $session,
+        LoginsRepository $loginsRepository
+    ): Response
     {
-        return $this->render('proj/home/home.html.twig');
+        $data = [];
+
+        if ($session->get("pokerUser")) {
+            $account = $loginsRepository->find(
+                $session->get("pokerUser")
+            );
+
+            if ($account->isAdmin()) {
+                $data = [
+                        "admin" => "true",
+                        "account" => $account->getUserName()
+                ];
+            } else {
+                $data = ["account" => $account->getUserName()];
+            }
+        }
+
+        return $this->render('proj/home/home.html.twig', $data);
     }
 
     /**
@@ -30,12 +55,14 @@ class ProjController extends AbstractController
      * )
      */
     public function home_post(
+        SessionInterface $session,
         Request $request
     ): Response
     {
         $logout = $request->request->get('logout');
 
         if ($logout) {
+            $session->clear();
             return $this->redirectToRoute('proj-home');
         }
     }
@@ -60,14 +87,28 @@ class ProjController extends AbstractController
      * )
      */
     public function game(
-        SessionInterface $session
+        SessionInterface $session,
+        LoginsRepository $loginsRepository
     ): Response
     {
         if (!$session->get("poker")) {
             $session->set("poker", new \App\Poker\Poker());
         }
 
-        $data = ["game" => $session->get("poker")];
+        if ($session->get("pokerUser")) {
+            $account = $loginsRepository->find(
+                $session->get("pokerUser")
+            );
+
+            $data = [
+                "game" => $session->get("poker"),
+                "account" => $account->getMoney()
+            ];
+        } else {
+            $data = ["game" => $session->get("poker")];
+        }
+
+        
 
         return $this->render('proj/game/game.html.twig', $data);
     }
@@ -80,7 +121,9 @@ class ProjController extends AbstractController
      * )
      */
     public function game_post(
+        ManagerRegistry $doctrine,
         SessionInterface $session,
+        LoginsRepository $loginsRepository,
         Request $request
     ): Response
     {
@@ -93,18 +136,151 @@ class ProjController extends AbstractController
         $poker = $session->get("poker");
 
         if ($bet) {
-            $poker->recieve_bet($request->request->get('bet'));
+            $money = $request->request->get('bet');
+            $poker->recieve_bet($money);
+
+            $entityManager = $doctrine->getManager();
+
+            $account = $loginsRepository->find($session->get("pokerUser"));
+            $account->setMoney($account->getMoney() - $money);
+
+            $entityManager->flush();
+
+            $history = new History();
+
+            $history->setLoginId($request->request->get("pokerUser"));
+            $history->setActs(
+                "Account-name: " .
+                $account->getUserName() .
+                ", has BET " .
+                $money . " money on poker"
+            );
+
+            $entityManager->persist($history);
+            $entityManager->flush();
+
             $poker->flop();
         } elseif ($fold) {
+            $entityManager = $doctrine->getManager();
+
+            $account = $loginsRepository->find($session->get("pokerUser"));
+            $account->setMoney($account->getMoney() - $poker->bet);
+
+            $history = new History();
+
+            $money = $poker->bet;
+
+            $history->setLoginId($request->request->get("pokerUser"));
+            $history->setActs(
+                "Account-name: " .
+                $account->getUserName() .
+                ", has FOLDED and lost " .
+                $money .
+                " money on poker "
+            );
+
+            $entityManager->persist($history);
+            $entityManager->flush();
+
             $poker->fold();
         } elseif ($again) {
             $poker->reset();
         } elseif ($call) {
+            $entityManager = $doctrine->getManager();
+
+            $account = $loginsRepository->find($session->get("pokerUser"));
+            $account->setMoney($account->getMoney() - $poker->bet);
+
+            $history = new History();
+
+            $money = $poker->bet;
+
+            $history->setLoginId($request->request->get("pokerUser"));
+            $history->setActs(
+                "Account-name: " .
+                $account->getUserName() .
+                ", has CALLED and bet another " .
+                $money .
+                " money on poker | " .
+                $money * 2 .
+                " money in total"
+            );
+
+            $entityManager->persist($history);
+            $entityManager->flush();
+
             $poker->call();
             $poker->rank_player();
         } elseif ($continue) {
             $poker->rank_dealer();
             $poker->compare_ranks();
+
+            if ($poker->phase == 4) {
+                $entityManager = $doctrine->getManager();
+
+                $account = $loginsRepository->find($session->get("pokerUser"));
+
+                $history = new History();
+                $money = $poker->bet;
+
+                $history->setLoginId($request->request->get("pokerUser"));
+                $history->setActs(
+                    "Account-name: " .
+                    $account->getUserName() .
+                    ", LOST in poker and lost a total of " .
+                    $money .
+                    " money"
+                );
+
+                $entityManager->persist($history);
+
+                $entityManager->flush();
+
+            }elseif ($poker->phase == 5) {
+                $entityManager = $doctrine->getManager();
+
+                $money = $poker->bet;
+
+                $account = $loginsRepository->find($session->get("pokerUser"));
+                $account->setMoney($account->getMoney() + $money);
+
+                $history = new History();
+
+                $history->setLoginId($request->request->get("pokerUser"));
+                $history->setActs(
+                    "Account-name: " .
+                    $account->getUserName() .
+                    ", DREW in poker and recieved " . 
+                    $money .
+                    " money back to thier wallet"
+                );
+
+                $entityManager->persist($history);
+
+                $entityManager->flush();
+            } elseif ($poker->phase == 6) {
+                $entityManager = $doctrine->getManager();
+
+                $money = $poker->bet * 2;
+
+                $account = $loginsRepository->find($session->get("pokerUser"));
+                $account->setMoney($account->getMoney() + ($money));
+
+                $history = new History();
+
+                $history->setLoginId($request->request->get("pokerUser"));
+                $history->setActs(
+                    "Account-name: " .
+                    $account->getUserName() .
+                    ", WON in poker and recieved " .
+                    $money .
+                    " money back to thier wallet"
+                );
+
+                $entityManager->persist($history);
+
+                $entityManager->flush();
+            }
         } elseif ($back) {
             $poker->reset();
             return $this->redirectToRoute('proj-home');
@@ -120,9 +296,44 @@ class ProjController extends AbstractController
      *      methods={"GET","HEAD"}
      * )
      */
-    public function login(): Response
+    public function login(
+    ): Response
     {
         return $this->render('proj/user/login.html.twig');
+    }
+
+    /**
+     *@Route(
+     *      "/proj/login",
+     *      name="proj-login-post",
+     *      methods={"POST"}
+     * )
+     */
+    public function login_post(
+        SessionInterface $session,
+        LoginsRepository $loginsRepository,
+        Request $request
+    ): Response
+    {
+        $login = $request->request->get('login');
+        $account_name = $request->request->get('account_name');
+        $password = $request->request->get('password');
+
+        if ($login) {
+            $account = $loginsRepository->findOneBy(
+                ["user_name" => $account_name]
+            );
+
+            if ($account->getPassword() == $password) {
+                $session->set("pokerUser", $account->getId());
+
+                return $this->redirectToRoute('proj-home');
+            }
+        }
+
+        $data = ["fail" => "Failed to login"];
+
+        return $this->redirectToRoute('proj-login-home', $data);
     }
 
     /**
@@ -135,5 +346,118 @@ class ProjController extends AbstractController
     public function register(): Response
     {
         return $this->render('proj/user/register.html.twig');
+    }
+
+    /**
+     *@Route(
+     *      "/proj/register",
+     *      name="proj-register-post",
+     *      methods={"POST"}
+     * )
+     */
+    public function register_post(
+        ManagerRegistry $doctrine,
+        Request $request
+    ): Response
+    {
+        $register = $request->request->get('register');
+        $account_name = $request->request->get('account_name');
+        $password = $request->request->get('password');
+
+        if ($register) {
+            $entityManager = $doctrine->getManager();
+
+            $login = new Logins();
+
+            $login->setUserName($account_name);
+            $login->setPassword($password);
+            $login->setMoney(1000);
+            $login->setAdmin(false);
+
+            $entityManager->persist($login);
+            $entityManager->flush();
+
+            $data = ["fail" => "Account created"];
+
+            return $this->redirectToRoute('proj-login-home', $data);
+        }
+
+        $data = ["fail" => "Account failed to creat"];
+
+        return $this->redirectToRoute('proj-register-home', $data);
+    }
+
+    /**
+     * @Route(
+     *      "/proj/reset",
+     *      name="proj-reset-home",
+     *      methods={"GET","HEAD"}
+     * )
+     */
+    public function reset(
+        ManagerRegistry $doctrine,
+        SessionInterface $session,
+        LoginsRepository $loginsRepository,
+        HistoryRepository $historyRepository
+    ): Response
+    {
+        $session->clear();
+
+        $data = [
+            "login" => $loginsRepository->findAll(),
+            "history" => $historyRepository->findAll()
+        ];
+
+        $entityManager = $doctrine->getManager();
+
+        foreach($data["login"] as $login) {
+            $entityManager->remove($login);
+        }
+
+        foreach($data["history"] as $history) {
+            $entityManager->remove($history);
+        }
+
+        $entityManager->flush();
+
+        $login = new Logins();
+
+        $login->setUserName("doe");
+        $login->setPassword("doe");
+        $login->setMoney(1000);
+        $login->setAdmin(false);
+
+        $entityManager->persist($login);
+
+        $login = new Logins();
+
+        $login->setUserName("admin");
+        $login->setPassword("admin");
+        $login->setMoney(1000000);
+        $login->setAdmin(true);
+
+        $entityManager->persist($login);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('proj-home');
+    }
+
+    /**
+     * @Route(
+     *      "/proj/history",
+     *      name="proj-history-home",
+     *      methods={"GET","HEAD"}
+     * )
+     */
+    public function history(
+        HistoryRepository $historyRepository
+    ): Response
+    {
+        $data = [
+            "history" => $historyRepository->findAll()
+        ];
+
+        return $this->render('proj/about/history.html.twig', $data);
     }
 }
